b4696d5c6ce3e99914a3e186095845d98c0798e2
Saul Ramos <seramos@cleveritgroup.com>
2024-11-08 17:34:53 -0400

    fix movie-card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { Movie } from '../interfaces/movie';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatChipsModule],
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.css'],
 })
 export class MovieCardComponent {
   @Input() movie!: Movie;
+
+  get genres(): string[] {
+    return this.movie.generes.split(',').map((genre) => genre.trim());
+  }
 }
